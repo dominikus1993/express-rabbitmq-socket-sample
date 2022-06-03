d3c25714a9885583ec8296d0a1a6819011989740
Dominik Kotecki <Dominik.Kotecki@rossmann.com.pl>
2022-06-03 07:54:46 +0000

    test

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,10 @@ import { TestMessage } from "./model/message";
 import { createAdapter } from "@socket.io/redis-adapter";
 import { createClient } from "redis";
 import { Emitter } from "@socket.io/redis-emitter";
+import path from "path";
 const logger = pino({level: process.env.NODE_ENV === "development" ? "debug": "warn"})
 const expresLogger = express_pino({logger: logger})
-const pubClient = createClient({ url: process.env.REDIS_CONNECTION ?? "redis://db:6379", name: "pub" });
+const pubClient = createClient({ url: process.env.REDIS_CONNECTION ?? "redis://db:6379"});
 const subClient = pubClient.duplicate();
 
 const app = express()
@@ -20,15 +21,8 @@ app.use(expresLogger)
 
 const bus = RabbitMqBus.from(process.env.RABBITMQ_CONNECTION ?? "amqp://guest:guest@rabbitmq:5672/")
 
-app.get('/:to', async function (req, res) {
-    await bus.publish<TestMessage>({exchange: "test", topic: "#", message: { to: req.params['to'] }})
-    res.send('send')
-  })
-
-
 const server = http.createServer(app);
-
-const io = new Server(server, { cors: { origin: '*' }}); // < Interesting!
+const io = new Server(server, { cors: { origin: '*' }}); 
 
 await Promise.all([pubClient.connect(), subClient.connect()]);
 io.adapter(createAdapter(pubClient, subClient));
@@ -38,6 +32,10 @@ await bus.consume({exchange: process.env.EXCHANGE ?? "test", queue: process.env.
     logger.info(`Hello ${msg.to}`)
 });
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(path.resolve(), path.join('src', 'index.html')));
+});
+
 io.on("connection", (socket) => {
     logger.info("New client connected");
     socket.on("disconnect", () => {
@@ -45,5 +43,13 @@ io.on("connection", (socket) => {
     });
   });
 
+
+server.on("error", (err) => {
+  logger.error(err);
+})
+
+server.on("close", () => {
+  logger.info("Server closed");
+})
 
 server.listen(process.env.PORT ?? 3000, () => { logger.info("Server is listening") })
